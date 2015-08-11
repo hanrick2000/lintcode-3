f302bac141f7b6c61e027573b239a7fe4fe11075
tianlinliu <tl1324@nyu.edu>
2015-08-11 13:14:02 -0700

    add FindFirstBadVersion.

---
+++ FindFirstBadVersion.java
@@ -0,0 +1,34 @@
+/**
+ * public class VersionControl {
+ *     public static boolean isBadVersion(int k);
+ * }
+ * you can use VersionControl.isBadVersion(k) to judge whether
+ * the kth code version is bad or not.
+*/
+class FindFirstBadVersion {
+    /**
+     * Time: O(logN)
+     * Space: O(1)
+     *
+     * @param n: An integers.
+     * @return: An integer which is the first bad version.
+     */
+    public int findFirstBadVersion(int n) {
+        int low = 0;
+        int high = n;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (VersionControl.isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        if (VersionControl.isBadVersion(low)) {
+            return low;
+        }
+        return high;
+    }
+}
+
