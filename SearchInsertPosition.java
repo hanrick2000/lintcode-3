5fc3312a44104e20969363091cad72b10dd7ebda
tianlinliu <tl1324@nyu.edu>
2015-08-17 16:54:16 -0700

    update SearchInsertPosition using discrete algorithm.

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -12,6 +12,23 @@ public class SearchInsertPosition {
 
         int low = 0;
         int high = A.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (A[mid] >= target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return A[low] >= target ? low : A.length;
+    }
+
+    // older
+    public int searchInsert(int[] A, int target) {
+        if (A == null || A.length == 0) return 0;
+
+        int low = 0;
+        int high = A.length - 1;
         while (low + 1 < high) {
             int mid = low + (high - low) / 2;
             if (A[mid] == target) {
