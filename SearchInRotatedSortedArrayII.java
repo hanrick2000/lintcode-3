c6436ab3a62c7b15daf8b0a8a624c28cc9ba3ea8
tianlinliu <tl1324@nyu.edu>
2015-08-18 01:20:50 -0700

    add SearchInRotatedSortedArrayII.

---
+++ SearchInRotatedSortedArrayII.java
@@ -0,0 +1,56 @@
+public class SearchInRotatedSortedArrayII {
+    /**
+     * Time: O(N)
+     * Space: O(1)
+     *
+     * param A : an integer ratated sorted array and duplicates are allowed
+     * param target :  an integer to be search
+     * return : a boolean
+     */
+    public boolean search(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return false;
+        }
+
+        int low = 0;
+        int high = A.length - 1;
+        while (low + 1 < high) {
+            int mid = low + (high - low) / 2;
+            if (A[low] == A[mid]) {
+                low++;
+            } else if (A[low] < A[mid]) {
+                if (A[low] <= target && target <= A[mid]) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (A[mid] <= target && target <= A[high]) {
+                    low = mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+            /*
+            if (A[mid] == A[high]) {
+                high--;
+            } else if (A[mid] < A[high]) {
+                if(A[mid] <= target && A[high] >= target) {
+                    low = mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (A[low] <= target && A[mid] >= target) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            */
+        }
+        return A[low] == target || A[high] == target;
+    }
+}
+
