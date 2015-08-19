9c6587b373c5311e6f69a714e1dbd68fa765f02c
tianlinliu <tl1324@nyu.edu>
2015-08-19 00:05:54 -0700

    add WoodCut

---
+++ WoodCut.java
@@ -0,0 +1,40 @@
+public class WoodCut {
+    /**
+     *@param L: Given n pieces of wood with length L[i]
+     *@param k: An integer
+     *return: The maximum length of the small pieces.
+     */
+    public int woodCut(int[] L, int k) {
+        if (L == null || L.length == 0) {
+            return 0;
+        }
+
+        int low = 1;
+        int high = findMax(L);
+        while (low < high) {
+            int mid  = low + (high - low + 1) / 2;
+            if (cut(L, mid) < k) {
+                high = mid - 1;
+            } else {
+                low = mid;
+            }
+        }
+        return cut(L, low) >= k ? low : 0;
+    }
+
+    private int findMax(int[] L) {
+        int max = L[0];
+        for (int n : L) {
+            max = Math.max(max, n);
+        }
+        return max;
+    }
+
+    private int cut(int[] L, int len) {
+        int sum = 0;
+        for (int n : L) {
+            sum += n / len;
+        }
+        return sum;
+    }
+}
