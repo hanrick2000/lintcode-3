336b8166773a22b700ec8b9a90333dcbf1c37032
tianlinliu <tl1324@nyu.edu>
2015-09-11 16:38:06 -0700

    update LargestRectangleInHistogram.
    add time.

--- LargestRectangleInHistogram.java
+++ LargestRectangleInHistogram.java
@@ -1,5 +1,7 @@
 public class LargestRectangleInHistogram {
     /**
+     * Time: O(N)
+     * Space: O(N)
      * @param height: A list of integer
      * @return: The area of largest rectangle in the histogram
      */
